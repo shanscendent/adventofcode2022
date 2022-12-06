70773f6df665e014e711c1c3d83086d37c35aa71
Lee, Shan Neng <shan.neng.lee@intel.com>
2022-12-06 14:04:55 +0800

    Day 6 solutions

---
+++ day06/part2.py
@@ -0,0 +1,19 @@
+import time
+import re
+
+t1 = time.time()
+
+n = 14
+
+with open('input.txt') as f:
+    buf = f.readline()
+
+for i in range(len(buf)- n + 1):
+    window = buf[i:i+n]
+    if len(set(window)) == n:
+        break
+
+print(i+n)
+
+t2 = time.time()
+print(t2-t1)
